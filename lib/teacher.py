219deeedd7352cd875e3a2e481ecdf48448401d2
sudomastery <roykoigu@protonmail.com>
2025-10-17 09:20:52 +0300

    Implement Student and Teacher classes with initialization and learning/teaching methods

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,23 @@ from user import User
 
 import random
 
+
 class Teacher(User):
+    """Teacher inherits from User and can teach random knowledge."""
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        # Return a random element from the knowledge list.
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
+
 class User:
-    pass
+    """Basic user with first and last name."""
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
